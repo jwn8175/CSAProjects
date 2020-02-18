789dcf377b1fa53b051a5df1091eb6f8a502460d
Jay <39040834+jayn0354@users.noreply.github.com>
2020-02-17 21:37:53 -0800

    Add files via upload

--- SpaceGame/BossLaser.java
+++ SpaceGame/BossLaser.java
@@ -1,7 +1,17 @@
 package SpaceGame;
 
+import java.awt.Color;
+
 public class BossLaser extends Laser {
 	
 	Laser[] lasers = new Laser[50];
 	
+	public BossLaser() {
+		for (int i = 0; i < lasers.length; i++) {
+			Laser tempLaser = new Laser();
+			tempLaser.theta = Math.PI/50 * i;
+			tempLaser.setColor(Color.BLUE);
+		}
+		
+	}
 }
